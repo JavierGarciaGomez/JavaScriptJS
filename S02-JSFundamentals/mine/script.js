2c895a7e67ec947adf39b8e6df9875c14959f019
JGG <javieron.garcia@gmail.com>
2021-01-15 03:37:08 +0100

    Section 02

---
+++ S02-JSFundamentals/mine/script.js
@@ -0,0 +1,57 @@
+console.log('***********************Lesson 7 Hello, World*******************************');
+// alert
+
+let js = 'amazing';
+
+if (js === 'amazing') {
+	console.log('Passed the if');
+}
+
+console.log(4 + 3 + 2);
+
+console.log('***********************Lesson 8 A brief introduction*******************************');
+/**JS is a highlevel, object oriented, multiparadigm programming language
+ * programming language: tool that let us write code
+ * high-level: we dont have to worry about complex stuff like memory management
+ * oop: let us use objects
+ * multi-paradigm: we can use multiple styles
+ * 
+ * What does it do?
+ * is the language that gives dynamic to html and css (verbs vs nouns and adjettives)
+ * 
+ * frontend
+ * backend
+ * native mobile app
+ * native desktop app
+ * 
+ * JavaScript releases
+ * ES5
+ * ES6 2015
+ * ES7 2016
+ * ES8 2017
+ * ES9 2018
+ * ES10 2019
+ * ES11 2020g
+ */
+
+console.log('**********Lesson 9 Linking a JS File**********');
+console.log('**********L10 Values and Variables**********');
+let firstName = 'Javier';
+console.log(firstName);
+
+// assignments
+let country = 'Mexico',
+	continent = 'America',
+	population = 120000000;
+console.log(country, continent, population);
+
+console.log('**********L12 Data types**********');
+// number
+// String
+// boolean
+// undefined: variable not yet defined
+// null: empty value
+// Symbol ES2015: unique value that cannot be change
+// BigInt ES2020: larger integers than the number type can hold
+
+// Dynamic typing in JS
