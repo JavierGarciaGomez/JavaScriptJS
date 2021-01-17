845b20b69677bfcbd283b0fe3e40b783a215a8f2
JGG <javieron.garcia@gmail.com>
2021-01-17 17:46:54 +0100

    Section 07 DOM

---
+++ S07-DOM/Guess-My-Number/mine/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**70. Project#1 Guess mu number
+ */
+
+/**71. What's DOM manipulation
+ * DOM: Document object model
+ * 
+ */
+
+/**72. Selecting and manipulating
+ */
+
+/*document.querySelector('.message').textContent = 'Changed with querySelector: 🎉 Correct Number!';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+/**73. Handling events
+ */
+
