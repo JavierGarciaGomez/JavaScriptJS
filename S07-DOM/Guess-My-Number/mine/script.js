a4be55bebb843e8fa15541ad924aa6dcbb6b4486
JGG <javieron.garcia@gmail.com>
2021-01-18 00:07:28 +0100

    Section 07 DOM: Guess game

--- S07-DOM/Guess-My-Number/mine/script.js
+++ S07-DOM/Guess-My-Number/mine/script.js
@@ -22,3 +22,50 @@ console.log(document.querySelector('.guess').value);
 /**73. Handling events
  */
 
+const guessField = document.querySelector('.guess');
+const checkBtn = document.querySelector('.check');
+const numberField = document.querySelector('.number');
+const msgField = document.querySelector('.message');
+const scoreField = document.querySelector('.score');
+const highScoreField = document.querySelector('.highscore');
+const againBtn = document.querySelector('.again');
+let highScore = 0;
+let score = 20;
+let randomNum = Math.floor(Math.random() * 20) + 1;
+
+console.log(randomNum);
+
+checkBtn.addEventListener('click', () => {
+	const capturedNum = Number(guessField.value);
+	if (!capturedNum || capturedNum > 20 || capturedNum < 0) {
+		msgField.textContent = '🚫 Not a valid number';
+		return;
+	}
+
+	console.log(randomNum, capturedNum);
+	if (randomNum === capturedNum) {
+		document.querySelector('body').classList.add('winnerBg');
+		numberField.textContent = randomNum;
+		highScore = highScore > score ? highScore : score;
+		highScoreField.textContent = highScore;
+		msgField.textContent = 'correct number';
+	} else {
+		if (score === 0) {
+			msgField.textContent = '🤔 You lost the game';
+			return;
+		}
+		score--;
+		scoreField.textContent = score;
+		msgField.textContent = randomNum > capturedNum ? '📉 Too low' : '📈 Too high';
+	}
+	console.log(capturedNum);
+});
+
+againBtn.addEventListener('click', () => {
+	score = 20;
+	scoreField.textContent = score;
+	document.querySelector('body').classList.remove('winnerBg');
+	msgField.textContent = 'Start guessing...';
+	guessField.value = '';
+	numberField.textContent = '?';
+});
