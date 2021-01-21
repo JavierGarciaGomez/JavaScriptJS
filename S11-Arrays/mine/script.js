00b652bbe96faeaf4df7bde74a5ebecdb01bdaf6
JGG <javieron.garcia@gmail.com>
2021-01-21 12:57:25 +0100

    Section 11 Arrays

---
+++ S11-Arrays/mine/script.js
@@ -0,0 +1,222 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/**
+ * 144. Creating DOM elements
+ */
+
+const displayMovements = function (movementsArray) {
+  // empty the container
+  containerMovements.innerHTML = '';
+
+  movementsArray.forEach(function (mov, i) {
+    // define the type of movement
+    const type = mov >= 0 ? 'deposit' : 'withdraw';
+    // html for each movement
+    const html = `
+      <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+      `;
+    // insert it in the container
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+// call the function
+displayMovements(account1.movements);
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+/**
+ * NOT RELATED TO THE BANKLIST PROECT
+ */
+
+/////////////////////////////////////////////////
+console.log('********** 140. Simple Array Methods **********');
+// Simple Array Methods
+let arr = ['a', 'b', 'c', 'd', 'e'];
+
+console.log('+++++ SLICE +++++');
+// SLICE
+console.log('Full array: ', arr);
+console.log('slice(2)', arr.slice(2));
+console.log('slice(2, 4)', arr.slice(2, 4));
+console.log('slice(-2)', arr.slice(-2));
+console.log('slice(-1)', arr.slice(-1));
+console.log('slice(1, -2)', arr.slice(1, -2));
+console.log('slice()', arr.slice());
+console.log('...arr', [...arr]);
+
+console.log('+++++ SPLICE +++++');
+// SPLICE
+// console.log(arr.splice(2));
+let spliced = arr.splice(-1);
+console.log('splice(-1). Spliced:', spliced, 'array ', arr);
+spliced = arr.splice(1, 2);
+console.log('splice(1, 2). Spliced:', spliced, 'array ', arr);
+
+console.log('+++++ REVERSE +++++');
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+console.log(letters.join(' - '));
+
+console.log('********** 141. Looping arrays: Foreach **********');
+///////////////////////////////////////
+// Looping Arrays: forEach
+const movements2 = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements2) {
+for (const [i, movement] of movements2.entries()) {
+  if (movement > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('---- FOREACH ----');
+movements2.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
+  } else {
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
+  }
+});
+/**
+ * You can not break a foreach, so in those cases is better to use for of
+ */
+console.log('********** 142. forEach with maps and sets **********');
+console.log('---- MAPS ----');
+const currencies2 = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies2.forEach(function (value, key, map) {
+  console.log(`${key}: ${value}`);
+});
+
+console.log('---- SETS ----');
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
+});
+
+console.log('********** 145. Coding challenge #1 **********');
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+function checkDogs(dogsJulia, dogsKate) {
+  dogsJulia = dogsJulia.splice(1, -1);
+  console.log(dogsJulia);
+}
+
+const dogsJulia = [3, 5, 2, 12, 7];
+const dogsKate = [4, 1, 15, 8, 3];
+checkDogs(dogsJulia, dogsKate);
